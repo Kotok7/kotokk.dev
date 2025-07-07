191868a97b0056ac3033792cd2c64dcb2c7977f8
Kotokk <stefan.kot02@onet.pl>
2025-07-07 23:55:47 +0200

    added github popup

--- index.php
+++ index.php
@@ -82,7 +82,9 @@ $translations = [
         'discord_note'     => 'Note: ',
         'discord_status'   => 'Status: ',
         'update_section'   => 'Changelog',
-        'changelog'        => '<ul><li>Added "donate me" option with crypto adresses</li><li>Added 404 not found site</li><li>Added new iOS screen time emulator to the main page</li><li>Added a small badge on the top</li><li>Added Binance crypto adress</li></ul>',
+        'github1'          => 'Now on GitHub',
+        'github2'          => 'View source and contribute',
+        'changelog'        => '<ul><li>Added "donate me" option with crypto adresses</li><li>Added 404 not found site</li><li>Added new iOS screen time emulator to the main page</li><li>Added a small badge on the top</li><li>Added Binance crypto adress</li><li>Added a popup informing that the site is on GitHub</li></ul>',
         'rewriting'        => 'Rewriting site to React...',
         'last_update'      => 'Updated: 7 July',
         'sleeping'         => [
@@ -169,7 +171,9 @@ $translations = [
         'discord_note'     => 'Notatka: ',
         'discord_status'   => 'Status: ',
         'update_section'   => 'Dziennik zmian',
-        'changelog'        => '<ul><li>Dodano opcje "wesprzyj mnie" z adresami crypto</li><li>Dodano stronę 404</li><li>Dodano nowy emulator czasu przed ekranem do strony głównej</li><li>Dodano małą plakietkę na górze strony</li><li>Dodano adres Binance do adresów krypto</li></ul>',
+        'changelog'        => '<ul><li>Dodano opcje "wesprzyj mnie" z adresami crypto</li><li>Dodano stronę 404</li><li>Dodano nowy emulator czasu przed ekranem do strony głównej</li><li>Dodano małą plakietkę na górze strony</li><li>Dodano adres Binance do adresów krypto</li><li>Dodano popup informujący o tym że strona jest na GitHubie</li></ul>',
+        'github1'          => 'Od teraz na GitHubie',
+        'github2'          => 'Zobacz kod i kontrybuuj',
         'rewriting'        => 'Przepisywanie strony do React...',
         'last_update'      => 'Uaktualnione: 7 Lipca',
         'sleeping'         => [
@@ -253,6 +257,19 @@ $storedNick = $_COOKIE['blog_nick'] ?? '';
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 </head>
 <body>
+      <div class="zx-notification-floating" id="zxGithubAnnouncementWidget">
+        <a href="#" class="zx-external-navigator" target="_blank" rel="noopener noreferrer">
+            <svg class="zx-repo-emblem" viewBox="0 0 24 24">
+                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+            </svg>
+        </a>
+        <div class="zx-message-wrapper">
+            <div class="zx-primary-text"><?= htmlspecialchars($t['github1'], ENT_QUOTES) ?></div>
+            <div class="zx-secondary-text"><a href="https://github.com/Kotok7/kotokk.dev"><?= htmlspecialchars($t['github2'], ENT_QUOTES) ?></a></div>
+        </div>
+        <button class="zx-dismiss-trigger" onclick="zxDismissRepoNotification()" aria-label="Close">×</button>
+    </div>
+    <script src="github.js"></script>
 <header>
   <a href="?lang=<?= $lang_code === 'pl' ? 'en' : 'pl' ?>">
     <img src="photos/<?= $lang_code === 'pl' ? 'united-states.png' : 'poland.png' ?>"

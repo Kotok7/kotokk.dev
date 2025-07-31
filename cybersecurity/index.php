174cb7257727d876717b82e6de90f61dc52193b2
Kotokk <stefan.kot02@onet.pl>
2025-07-31 08:29:45 +0200

    small fix

--- cybersecurity/index.php
+++ cybersecurity/index.php
@@ -157,7 +157,7 @@ $t = $translations[$lang_code] ?? $translations['en'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($t['title'], ENT_QUOTES) ?></title>
     <meta name="description" content="<?= htmlspecialchars($t['meta_description'], ENT_QUOTES) ?>" />
-    <link rel="icon" type="image/png" href="website-icon.jpg" />
+    <link rel="icon" type="image/png" href="photos/website-icon.png" />
     <link rel="stylesheet" href="styles.css" />
 </head>
 <body>

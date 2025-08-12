1d8dc620a8f174565d108058e296b891fe6f99a8
Kotokk <stefan.kot02@onet.pl>
2025-08-13 00:19:49 +0200

    bug fix
    changed css

--- share-games/popup.js
+++
@@ -1,7 +0,0 @@
-if (!localStorage.getItem('alertShown')) {
-    alert(
-      'CSS will be changed soon.\n' +
-      'CSS zostanie zmieniony wkrótce.'
-    );
-    localStorage.setItem('alertShown', 'true');
-}

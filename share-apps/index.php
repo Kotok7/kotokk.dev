e3abc45def7cd26b6edf80ab9d1f4d4a95f09223
Kotokk <stefan.kot02@onet.pl>
2025-08-16 14:18:41 +0200

    site loads in english by default

--- share-apps/index.php
+++ share-apps/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'translations.php';
-$lang = 'pl';
+$lang = 'en';
 if (isset($_GET['lang']) && in_array($_GET['lang'], ['pl','en'])) {
     $lang = $_GET['lang'];
     setcookie('lang', $lang, time()+30*24*3600, '/');
